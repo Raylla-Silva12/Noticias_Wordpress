8002ac8d965204689ca025172f34cca6cd544b9c
Rafa-Pinheiro <rafaela.spinheiros@gmail.com>
2022-05-16 23:21:04 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php get_header(); ?>
+
+    <h2>Notícias de Diárias</h2>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img  src ="<?php  echo  get_template_directory_uri()?>/assets/img/lasanha.jpg" class ="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Lasanheta anuncia!</h5>
+                        <p class="card-text">A promoção "100 lasanhas por 1 real" se trata de uma fraude. Os consumidores devem ficar alerta.</p>
+                        <a href="#" class="btn btn-primary">Leia mais</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?php  echo  get_template_directory_uri()?>/assets/img/acerola.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Descubra o Segredo</h5>
+                        <p class="card-text">Especialista desvenda o segredo emagrecimento rápido. Acerola.</p>
+                        <a href="#" class="btn btn-primary">Veja mais</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?php  echo  get_template_directory_uri()?>/assets/img/pizza_napolitana.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Pizzaria Zumbi</h5>
+                        <p class="card-text">Foi descoberto pela polícia uma facção criminosa perigosa. Pizzaria Zumbi vendia pizza de maconha deliciosa.</p>
+                        <a href="#" class="btn btn-primary">Veja mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+                <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?php  echo  get_template_directory_uri()?>/assets/img/maracuja.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Maracujá, A maravilha</h5>
+                        <p class="card-text">A Oitava Maravilha do mundo foi declada, se chama Maracujá. Houve um estudo a cerca e todos os especialista concordaram.</p>
+                        <a href="#" class="btn btn-primary">Veja mais</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?php  echo  get_template_directory_uri()?>/assets/img/sorvete.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Sorvete misterioso</h5>
+                        <p class="card-text">A Network anuncia que quem acertar o sabor deste sorvete, será premiado, com algo equivalente a 100 mil.</p>
+                        <a href="#" class="btn btn-primary">Veja mais</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?php  echo  get_template_directory_uri()?>/assets/img/pao_queijo.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Alerta a saúde</h5>
+                        <p class="card-text">O consumo excessivo de alimentos derivados de leite, leva diversas pessoas a intolerância a lactose.</p>
+                        <a href="#" class="btn btn-primary">Veja mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
